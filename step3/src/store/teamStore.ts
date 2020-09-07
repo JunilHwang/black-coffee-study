a8df8598a908de6c09f6d185b717e33e0f2e091c
junilhwang <tjsdlf4261@naver.com>
2020-09-08 00:16:34 +0900

    feat: [TodoList] Team Name 출력 완료

--- step3/src/store/teamStore.ts
+++ step3/src/store/teamStore.ts
@@ -1,47 +1,48 @@
-import {Store} from "@/core";
-import {TeamService} from "@/services";
-import {TodoTeam} from "@/domains";
+import {Store} from "../core/Store.js";
+import TeamService from "../services/TeamService.js";
 
 export const SET_TEAMS = 'SET_TEAMS';
+export const SET_TEAM = 'SET_TEAM';
+export const SET_OPENED_APPEND_FORM = 'SET_OPENED_APPEND_FORM';
 export const FETCH_TEAMS = 'FETCH_TEAMS';
+export const FETCH_TEAM = 'FETCH_TEAM';
 export const ADD_TEAM = 'ADD_TEAM';
-export const SET_OPENED_TEAM_APPEND_FORM = 'SET_OPENED_TEAM_APPEND_FORM';
 
-export interface TeamState {
-  teams: TodoTeam[];
-  openedAppendForm: boolean;
-}
-
-export const teamStore = new Store<TeamState>({
+export const teamStore = new Store({
 
   state: {
     teams: [],
+    team: {
+      _id: null,
+      name: null,
+      members: []
+    },
     openedAppendForm: false,
   },
 
   mutations: {
-
-    [SET_TEAMS] (state, teams: TodoTeam[]) {
+    [SET_TEAMS] (state, teams) {
       state.teams = teams;
     },
-
-    [SET_OPENED_TEAM_APPEND_FORM] (state, openedAppendForm: boolean) {
+    [SET_TEAM] (state, team) {
+      state.team = team;
+    },
+    [SET_OPENED_APPEND_FORM] (state, openedAppendForm) {
       state.openedAppendForm = openedAppendForm;
     },
-
   },
 
   actions: {
-
     async [FETCH_TEAMS] ({ commit }) {
       commit(SET_TEAMS, await TeamService.fetchTeams());
     },
-
-    async [ADD_TEAM] ({ dispatch }, name: string) {
+    async [FETCH_TEAM] ({ commit }, id) {
+      commit(SET_TEAM, await TeamService.fetchTeam(id));
+    },
+    async [ADD_TEAM] ({ dispatch }, name) {
       await TeamService.addTeam(name);
       return dispatch(FETCH_TEAMS);
     },
-
   },
 
 });
