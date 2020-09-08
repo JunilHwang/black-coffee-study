69b302cd7f3e3c3399bf82d30cfdb0f45123ec1c
junilHwang <tjsdlf4261@naver.com>
2020-09-08 18:26:40 +0900

    feat: addEventBubblingListener 추가

--- step1/src/utils/index.js
+++ step1/src/utils/index.js
@@ -6,3 +6,12 @@ export const debounceOneFrame = callback => {
     target = setTimeout(() => callback(), ONE_FRAME);
   }
 };
+
+export const addEventBubblingListener = (eventType, parent, childSelector, callback) => {
+  const isChildrenOf = target => [ ...parent.closest(childSelector) ].includes(target) ||
+                                 target.closest(childSelector)
+  parent.addEventListener(eventType, event => {
+    if (!isChildrenOf(event.target)) return;
+    callback(event);
+  })
+}
