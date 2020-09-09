30a4761830caf16b62e397a4300c729442dd0aad
junilHwang <tjsdlf4261@naver.com>
2020-09-09 23:41:32 +0900

    feat: addEventBubblingListener 추가

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+export const ONE_FRAME = 1000 / 60;
+export const debounceOneFrame = callback => {
+  let timer = null;
+  return props => {
+    clearTimeout(timer);
+    timer = setTimeout(() => callback(props), ONE_FRAME);
+  }
+}
+
+export const lazyFrame = () => new Promise(resolve => setTimeout(resolve, ONE_FRAME * 10))
+
+export const addEventBubblingListener = (eventType, currentTarget, selector, callback) => {
+  const isChild = target => [ ...currentTarget.querySelectorAll(selector) ].includes(target) ||
+                            target.closest(selector);
+  currentTarget.addEventListener(eventType, event => {
+    if (!isChild(event.target)) return;
+    callback(event);
+  })
+}
