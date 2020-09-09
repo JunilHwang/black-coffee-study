133e0a606cea49acc9656b7461e3ad097eeb1577
junilHwang <tjsdlf4261@naver.com>
2020-09-09 18:34:31 +0900

    Upstream Merge

--- step2/src/store/userStore.js
+++ step2/src/store/userStore.js
@@ -1,20 +1,18 @@
 import { Store } from '../core/Store.js';
 import TodoService from "../services/TodoService.js";
-import {getQuery} from "../utils/index.js";
 
 export const SET_USERS = 'SET_USERS';
 export const SET_USER = 'SET_USER';
 export const FETCH_USERS = 'FETCH_USERS';
-export const ADD_USER = 'ADD_USER';
 
 export const userStore = new Store({
   state: {
     users: [],
-    selectedIndex: 0,
+    selectedIndex: -1,
   },
 
   getters: {
-    selectedUser: ({ users, selectedIndex }) => users[selectedIndex],
+    selectedUserName: ({ users, selectedIndex }) => users[selectedIndex],
   },
 
   mutations: {
@@ -28,13 +26,7 @@ export const userStore = new Store({
 
   actions: {
     async [FETCH_USERS] ({ commit }) {
-      const users = await TodoService.fetchUsers();
-      commit(SET_USERS, users);
-      return users;
-    },
-    async [ADD_USER] ({ dispatch }, name) {
-      await TodoService.addUser(name);
-      dispatch(FETCH_USERS);
+      commit(SET_USERS, await TodoService.fetchUsers());
     },
   }
 })
