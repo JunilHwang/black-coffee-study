2436f36cd22c99670aa2d030c2190e9173e9d80f
junilHwang <tjsdlf4261@naver.com>
2020-09-09 23:45:22 +0900

    feat: Component에 create component 추가

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,13 +1,16 @@
+import {addEventBubblingListener} from "../utils";
+
+const components = {};
+
 export const defineComponent = ({ name, propsKeys = [], setEvent = () => {} }, render) => {
 
-  customElements.define(name, class extends HTMLElement {
+  components[name] = class extends HTMLElement {
 
     $props; $state;
 
     constructor() {
       super();
-
-      this.$state = state;
+;
       this.$props = propsKeys.reduce((obj, key) => {
         Object.defineProperty(obj, key, {
           get: () => this.getAttribute(key),
@@ -16,12 +19,8 @@ export const defineComponent = ({ name, propsKeys = [], setEvent = () => {} }, r
         return obj;
       }, {});
 
-      this.innerHTML = render({
-        state: this.$state,
-        props: this.$props,
-      });
-
-      setEvent();
+      setEvent(this);
+      this.#render();
     }
 
     static get observedAttributes() {
@@ -29,18 +28,43 @@ export const defineComponent = ({ name, propsKeys = [], setEvent = () => {} }, r
     }
 
     attributeChangedCallback() {
+      this.#render();
+    }
+
+    #render () {
       this.innerHTML = render({
         state: this.$state,
         props: this.$props,
       });
     }
 
-  });
+    setup ({ state }) {
+      this.setState(state);
+      return this;
+    }
+
+    setState (payload) {
+      this.$state = { ...this.$state, ...payload };
+      this.#render();
+    }
+
+    addEvent (eventType, ref, callback) {
+      addEventBubblingListener(eventType, this, `[data-ref="${ref}"]`, callback);
+      return this;
+    }
+
+  }
+
+  customElements.define(name, components);
 
 }
 
-export const createComponent = () => {
-
+export const createComponent = (name, { state }) => {
+  if (components[name] === undefined) {
+    throw new Error(`${name} component는 존재하지 않습니다.`);
+  }
+  return document.createElement(name)
+                 .setup({ state });
 }
 
 export default { defineComponent, createComponent };
