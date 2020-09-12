4f4c6a8d8dd9c2c5641e0add445e64149a16eda5
junilHwang <tjsdlf4261@naver.com>
2020-09-12 23:42:31 +0900

    rebase: 합치는 중

--- step2/src/store/userStore.js
+++ step2/src/store/userStore.js
@@ -1,5 +1,6 @@
 import { Store } from '../core/Store.js';
 import TodoService from "../services/TodoService.js";
+import {getQuery} from "../utils/index.js";
 
 export const SET_USERS = 'SET_USERS';
 export const SET_USER = 'SET_USER';
@@ -25,8 +26,17 @@ export const userStore = new Store({
   },
 
   actions: {
-    async [FETCH_USERS] ({ commit }) {
-      commit(SET_USERS, await TodoService.fetchUsers());
+    [FETCH_USERS] ({ commit }) {
+      return new Promise(async resolve => {
+        const users = await TodoService.fetchUsers();
+        const userId = getQuery('user_id');
+        console.log(userId);
+        const selectedIndex = users.findIndex(({ _id }) => _id === userId);
+        console.log(selectedIndex);
+        commit(SET_USERS, users);
+        commit(SET_USER, selectedIndex);
+        resolve();
+      })
     },
   }
 })
