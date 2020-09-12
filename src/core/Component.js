a9620cc99405f3c953c3a8f042621043b2c77273
junilHwang <tjsdlf4261@naver.com>
2020-09-12 21:53:18 +0900

    refactor: Component 재작성

---
+++ src/core/Component.js
@@ -0,0 +1,44 @@
+import { debounceOneFrame } from "../utils/index.js";
+
+export const Component = class {
+
+  $target; $state = {}; $props = {}; $children;
+
+  constructor(target, state, props) {
+    this.$props = props;
+    this.componentInit();
+    this.render();
+    this.componentDidMount();
+    this.setEvent();
+  }
+
+  componentInit () {}
+  componentDidUpdate () {}
+  componentDidMount () {}
+  setEvent () {}
+  template () { return '' }
+
+  render = debounceOneFrame(() => {
+    this.$target.innerHTML = this.template();
+    this.#childrenBuild();
+    this.componentDidUpdate()
+  });
+
+  setState (payload) {
+    this.$state = { ...this.$state, ...payload };
+    this.render();
+  }
+
+  addEvent () {
+
+  }
+
+
+  #childrenBuild () {
+    this.$target.querySelectorAll('[data-component]').forEach(({ target}) => {
+      const { constructor, props } = this.$children[target.dataset.component];
+      new constructor(target, props);
+    })
+  }
+
+}
