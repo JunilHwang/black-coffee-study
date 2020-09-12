472595cf95d57c2f5f32574cd5563cbcfa6b4e7f
junilHwang <tjsdlf4261@naver.com>
2020-09-12 21:13:47 +0900

    refactor: Component Core 다시 작성 완료

--- src/core/Component.js
+++ src/core/Component.js
@@ -2,4 +2,23 @@ import {addEventBubblingListener} from "../utils";
 
 export const Components = class {
 
+  $state; $target; $props;
+
+  constructor($target, $props) {
+    this.$target = $target;
+    this.$props = $props;
+    this.componentInit();
+    this.render();
+    this.componentDidMount();
+  }
+
+  componentInit () {}
+  componentDidMount () {}
+  componentDidUpdate () {}
+  template () {}
+  render () {
+    this.$target.innerHTML = this.template();
+    this.componentDidUpdate();
+  }
+
 }
