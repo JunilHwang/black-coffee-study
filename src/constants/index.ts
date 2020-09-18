5a264b12f51a11cd4802131f59a5742745dd5741
junilHwang <tjsdlf4261@naver.com>
2020-09-18 11:36:16 +0900

    refactor: core/index.ts 추가 및 Import 구문에 적용

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,8 +6,15 @@ export const enum HttpMethod {
   DELETE = 'DELETE',
 }
 
-export const enum Priority {
+export const enum PriorityTypes {
   NONE,
   PRIMARY,
   SECONDARY
+}
+
+export const enum FilterTypes {
+  ALL = 'all',
+  PRIORITY = 'priority',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
 }
