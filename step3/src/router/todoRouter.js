414e8e5fcc986f300545985cd3d1bdbc90afb3b8
junilHwang <tjsdlf4261@naver.com>
2020-09-19 13:50:27 +0900

    refactor: typescript build 성공

--- step3/src/router/todoRouter.js
+++ step3/src/router/todoRouter.js
@@ -1,12 +1,13 @@
 import {Router} from "@/core";
 import {Team, Kanban} from "@/containers";
 
-const $app = document.querySelector('#app');
-export const todoRouter = new Router((uri?: string) => {
-  if (uri?.includes('index') || uri?.length === 0) {
+const $app = document.querySelector('#app') as HTMLElement;
+
+export const todoRouter = new Router((uri: string) => {
+  if (uri.includes('index') || uri.length === 0) {
     return new Team($app);
   }
-  if (uri?.includes('kanban')) {
+  if (uri.includes('kanban')) {
     return new Kanban($app);
   }
 });
