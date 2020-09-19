89420044fe2de577533f7c1f05e035140139b20b
junilHwang <tjsdlf4261@naver.com>
2020-09-19 15:23:45 +0900

    refactor: Event 타입 적용 중

--- src/domains/index.ts
+++ src/domains/index.ts
@@ -1,11 +1,8 @@
 import {Component} from "@/core";
-import {PriorityTypes} from "@/constants";
 
 export type RequestQuery = Record<string, string>;
 
 export type RequestBodyValue = string | boolean | number | null | undefined;
-
-export type Events = Event | KeyboardEvent | MouseEvent | undefined;
 
 export type RequestBody = Record<string, RequestBodyValue | RequestBodyValue[] | Record<string, RequestBodyValue>>;
 
@@ -14,6 +11,17 @@ export interface Constructable<T> {
 }
 
 export type ComponentConstructable = Constructable<Component<any>>;
+
+export interface CommonEvent<T extends HTMLElement = HTMLElement> extends Omit<Event, 'target'> {
+  target: T
+}
+
+export interface KeyEvent<T extends HTMLInputElement = HTMLInputElement> extends Omit<KeyboardEvent, 'target'|'key'> {
+  target: T;
+  key: string;
+}
+
+export type PickEvent<T> = Extract<CommonEvent | KeyEvent, T>
 
 export * from "./TodoTeam";
 export * from "./TodoMember";
