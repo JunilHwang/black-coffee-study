348efcb49a9a091a89e1719557a2f4562e18db83
junilHwang <tjsdlf4261@naver.com>
2020-09-19 16:42:20 +0900

    rebase: step3 rebase 적용 중 (2)

---
+++ step3/src/services/TeamService.ts
@@ -0,0 +1,25 @@
+import { todoAdapterClient } from "@/adapter/todoAdapter";
+
+export default Object.freeze({
+
+  fetchTeams () {
+    return todoAdapterClient.get('teams');
+  },
+
+  fetchTeam (teamId: string) {
+    return todoAdapterClient.get(`teams/${teamId}`);
+  },
+
+  addTeam (name: string) {
+    return todoAdapterClient.post(`teams`, { name });
+  },
+
+  addTeamMember (teamId: string, name: string) {
+    return todoAdapterClient.post(`teams/${teamId}/members`, { name });
+  },
+
+  deleteTeam (teamId: string) {
+    return todoAdapterClient.delete(`teams/${teamId}`);
+  },
+
+})
