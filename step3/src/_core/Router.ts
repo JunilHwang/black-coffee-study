4c41e2151469478286340fc50c84fc376e37a13f
junilhwang <tjsdlf4261@naver.com>
2020-12-23 15:06:04 +0900

    refactor: Router 코드를 해시 기반으로 변

--- step3/src/_core/Router.ts
+++ step3/src/_core/Router.ts
@@ -6,22 +6,24 @@ export const Router = class {
   public $query: RequestQuery = {};
 
   constructor (
-    private readonly callback: (uri: string) => void
+    private readonly callback: (uri: string) => void,
+    private readonly baseURI: string = ""
   ) {
     window.onpopstate = () => this.load();
   }
 
   public load (): void {
-    const uri: string = location.pathname.split('/').pop() || '';
-    this.$query = parseQuery(location.search);
+    const uri: string = location.href.replace(location.origin, "");
+    this.$query = parseQuery(uri);
     this.callback(uri);
   }
 
   public push (uri: string): void {
+    const fullUri = `${this.baseURI}/#!${uri}`;
     const query: RequestQuery = parseQuery(uri);
     this.$query = query;
-    this.callback(uri);
-    history.pushState(query, '', uri);
+    this.callback(fullUri);
+    history.pushState(query, '', fullUri);
   }
 
 }
